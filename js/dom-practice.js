b77936558481db9f6d1b55fa5404aa136a37d609
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-02 16:23:35 -0700

    Step 4 Completed

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -7,19 +7,25 @@ function alertMe() {
 }
 
 //STEP 2
-let myElement = window.document.getElementById("myButton");
+let myElement = window.document.getElementById("step2");
 
-myElement.onclick = function (){
+myElement.onclick = function () {
     window.alert("I have been clicked");
 }
 
 //STEP 3
-let myBtn = window.document.getElementById("myBtn");
+let myBtn = window.document.getElementById("step3");
 myBtn.addEventListener("click", function () {
     window.alert("I have been clicked")
 });
 
 //STEP 4
+function message() {
+    window.alert("I have been clicked")
+}
+
+let step4 = window.document.getElementById("step4");
+step4.addEventListener("click", message);
 
 //STEP 5
 
