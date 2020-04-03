cad0e7ad06a62a1b58dc57473bf896eaf4e1070a
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-02 19:15:37 -0700

    Step 9 Complete

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -39,20 +39,34 @@ document.getElementById("redirect").addEventListener("click", function (e) {
 });
 
 //STEP 7
-document.getElementById("step7").addEventListener("click", function (){
-    this.disabled="true";
+document.getElementById("step7").addEventListener("click", function () {
+    this.disabled = "true";
     let userInput = document.getElementById("tBox");
     alert(userInput.value);
-   
+
 });
 
 //STEP 8
+document.getElementById('step8').addEventListener("click", function () {
+    window.open('newpage.html', 'New Page', "width=300px,height=300px;")
 
-document.getElementById('step8').addEventListener("click", function() {
-    window.open('newpage.html','New Page', "width=300px,height=300px;")
-  
 });
 
 //STEP 9
+
+let $ = (id) => {
+    return window.document.getElementById(id);
+}
+$("start9").addEventListener("click", function () {
+    let interval = setInterval(function () {
+        console.log('Learning JavScript is fun!')
+        console.log(' ');
+    }, 2000);
+    $("stop9").addEventListener("click", function () {
+        window.clearInterval(interval);
+    });
+
+});
+
 
 //STEP 10
